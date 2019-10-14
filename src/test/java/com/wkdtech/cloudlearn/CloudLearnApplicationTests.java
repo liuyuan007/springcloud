690ecda202756d792005f4022f3b5b07afab9240
liuyuan007 <570051296@qq.com>
2019-10-14 22:19:06 +0800

    初次提交项目

---
+++ src/test/java/com/wkdtech/cloudlearn/CloudLearnApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wkdtech.cloudlearn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudLearnApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
